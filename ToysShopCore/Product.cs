0e996a48bb15a61e1c0b9a3d2025096fd8f77c76
1318PK <ainur.hak2004@gmail.com>
2022-03-26 08:49:53 +0300

    Добавьте файлы проекта.

---
+++ ToysShopCore/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToysShopCore
+{
+    class Product
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Distributor { get; set; }
+        string Price { get; set; }
+        bool Actually { get; set; }
+        int Count { get; set; }
+
+        public Product(string str)
+        {
+            var data = str.Split(';');
+            Name = data[0];
+            Description = data[1];
+            Distributor = data[2];
+            Price = data[3];
+            Actually = bool.Parse(data[4]);
+            Count = int.Parse(data[5]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name};{Description};{Distributor};{Price};{Actually};{Count}";
+        }
+    }
+}
---
+++ ToysShopCore/ProductStorage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToysShopCore
+{
+    class ProductStorage
+    {
+        List<Product> Products { get; set; }
+
+        public ProductStorage()
+        {
+            Products = new List<Product>();
+        }
+
+        public void ReadFromFile(string path)
+        {
+            Products.Clear();
+            try
+            {
+                using (var reader = new StreamReader(path))
+                {
+                    string str;
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        Products.Add(new Product(str));
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        public void WriteInFile(string path)
+        {
+            using (var writer = new StreamWriter(path, false))
+            {
+                foreach (var product in Products)
+                {
+                    writer.WriteLine(product);
+                }
+            }
+        }
+    }
+}
